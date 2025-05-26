d1ac8def06d9c4b96d67f4ca44eb2a700cdb0033
kimsungwon318 <sungwon.kim@narimoto.co.jp>
2025-05-26 23:44:09 +0900

    file upload

--- components/file-dragdropzone.tsx
+++ components/file-dragdropzone.tsx
@@ -1,9 +1,23 @@
 "use client"
 
+import { Button } from "@material-tailwind/react";
+import { useRef } from "react"
+
 export default function FileDragDropZone() {
+
+    const fileRef = useRef(null)
+
     return (
-        <section className="w-full border-2 border-dotted border-indigo-700">
-            <input type="file" className="w-full py-20" />
-        </section>
+        <form 
+            onSubmit={(e) => {
+                e.preventDefault();
+                const file = fileRef.current.files?.[0];
+                console.log(file);
+            } }
+            className="w-full py-20 border-4 border-dotted border-indigo-700 flex flex-col items-center justify-center">
+            <input ref={fileRef} type="file" className="" />
+            <p>파일을 여기에 끌어다 놓거나 클릭하여 업로드하세요.</p>
+            <Button type="submit">파일 업로드</Button>
+        </form>
     )
 }
